e0b7b9348a968403b61edca87d6ee5dfa67e99ee
Zak Henry <zak@cloudnc.com>
2018-09-20 18:25:41 +0100

    feat(JS): Adds index.js/d.ts so clients can import from a single package

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,13 @@
  * specified in [plugin.proto](https://github.com/google/protobuf/blob/master/src/google/protobuf/compiler/plugin.proto).
  */
 import {ExportMap} from "./lib/ExportMap";
+import { ProtoIndexFormatter } from './lib/format/ProtoIndexFormatter';
 import {Utility} from "./lib/Utility";
 import {CodeGeneratorRequest, CodeGeneratorResponse} from "google-protobuf/google/protobuf/compiler/plugin_pb";
 import {FileDescriptorProto} from "google-protobuf/google/protobuf/descriptor_pb";
 
 import {ProtoMsgTsdFormatter} from "./lib/format/ProtoMsgTsdFormatter";
 import {ProtoSvcTsdFormatter} from "./lib/format/ProtoSvcTsdFormatter";
-import { TplEngine } from "./lib/TplEngine";
 
 Utility.withAllStdIn((inputBuff: Buffer) => {
 
@@ -30,27 +30,44 @@ Utility.withAllStdIn((inputBuff: Buffer) => {
             exportMap.addFileDescriptor(protoFileDescriptor);
         });
 
-        codeGenRequest.getFileToGenerateList().forEach(fileName => {
-            // message part
-            let msgFileName = Utility.filePathFromProtoWithoutExt(fileName);
-            let msgTsdFile = new CodeGeneratorResponse.File();
-            msgTsdFile.setName(msgFileName + ".d.ts");
-            const msgModel = ProtoMsgTsdFormatter.format(fileNameToDescriptor[fileName], exportMap);
-            msgTsdFile.setContent(TplEngine.render('msg_tsd', msgModel));
-            codeGenResponse.addFile(msgTsdFile);
+        const files = codeGenRequest.getFileToGenerateList().reduce((fileList, fileName) => {
+          // message part
+          let msgFileName = Utility.filePathFromProtoWithoutExt(fileName);
+          fileList.push(msgFileName);
+          let msgTsdFile = new CodeGeneratorResponse.File();
+          msgTsdFile.setName(msgFileName + ".d.ts");
+          msgTsdFile.setContent(ProtoMsgTsdFormatter.format(fileNameToDescriptor[fileName], exportMap));
+          codeGenResponse.addFile(msgTsdFile);
 
-            // service part
-            let fileDescriptorModel = ProtoSvcTsdFormatter.format(fileNameToDescriptor[fileName], exportMap);
-            if (fileDescriptorModel != null) {
-                let svcFileName = Utility.svcFilePathFromProtoWithoutExt(fileName);
-                let svtTsdFile = new CodeGeneratorResponse.File();
-                svtTsdFile.setName(svcFileName + ".d.ts");
-                svtTsdFile.setContent(TplEngine.render('svc_tsd', fileDescriptorModel));
-                codeGenResponse.addFile(svtTsdFile);
-            }
-        });
+          // service part
+          let fileDescriptorOutput = ProtoSvcTsdFormatter.format(fileNameToDescriptor[fileName], exportMap);
+          if (fileDescriptorOutput != '') {
+              let svcFileName = Utility.svcFilePathFromProtoWithoutExt(fileName);
+              let svtTsdFile = new CodeGeneratorResponse.File();
+              svtTsdFile.setName(svcFileName + ".d.ts");
+              svtTsdFile.setContent(fileDescriptorOutput);
+              codeGenResponse.addFile(svtTsdFile);
+              fileList.push(svcFileName);
+          }
 
-        process.stdout.write(new Buffer(codeGenResponse.serializeBinary()));
+          return fileList;
+
+      }, []);
+
+      const indexJsOutput = ProtoIndexFormatter.format(files, 'index_js');
+      const indexJsFile = new CodeGeneratorResponse.File();
+      indexJsFile.setName("index.js");
+      indexJsFile.setContent(indexJsOutput);
+      codeGenResponse.addFile(indexJsFile);
+
+      const indexTsOutput = ProtoIndexFormatter.format(files, 'index_tsd');
+      const indexTsdFile = new CodeGeneratorResponse.File();
+      indexTsdFile.setName("index.d.ts");
+      indexTsdFile.setContent(indexTsOutput);
+      codeGenResponse.addFile(indexTsdFile);
+
+      process.stdout.write(new Buffer(codeGenResponse.serializeBinary()));
+
     } catch (err) {
         console.error("protoc-gen-ts error: " + err.stack + "\n");
         process.exit(1);
