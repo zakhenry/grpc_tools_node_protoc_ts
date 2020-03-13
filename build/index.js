481cbf508aac1195347fbf7533b91d577d4fe18e
Xinan Tao <xinan@cloudnc.com>
2020-03-13 17:45:20 +0000

    generate models mono-entry

--- build/index.js
+++ build/index.js
@@ -33,12 +33,12 @@ Utility_1.Utility.withAllStdIn((inputBuff) => {
             let msgFileName = Utility_1.Utility.filePathFromProtoWithoutExt(fileName);
             fileList.push(msgFileName);
             const msgInterfaceFile = new plugin_pb_1.CodeGeneratorResponse.File();
-            msgInterfaceFile.setName(msgFileName + ".interface.ts");
-            interfaces.push(msgFileName + ".interface");
+            msgInterfaceFile.setName(msgFileName + '.interface.ts');
+            interfaces.push(msgFileName + '.interface');
             msgInterfaceFile.setContent(ProtoMsgInterfaceFormatter_1.ProtoMsgInterfaceFormatter.format(fileNameToDescriptor[fileName], exportMap));
             codeGenResponse.addFile(msgInterfaceFile);
             let msgTsdFile = new plugin_pb_1.CodeGeneratorResponse.File();
-            msgTsdFile.setName(msgFileName + ".d.ts");
+            msgTsdFile.setName(msgFileName + '.d.ts');
             const msgModel = ProtoMsgTsdFormatter_1.ProtoMsgTsdFormatter.format(fileNameToDescriptor[fileName], exportMap);
             msgTsdFile.setContent(TplEngine_1.TplEngine.render('msg_tsd', msgModel));
             codeGenResponse.addFile(msgTsdFile);
@@ -47,7 +47,7 @@ Utility_1.Utility.withAllStdIn((inputBuff) => {
             if (fileDescriptorModel != null) {
                 let svcFileName = Utility_1.Utility.svcFilePathFromProtoWithoutExt(fileName);
                 let svtTsdFile = new plugin_pb_1.CodeGeneratorResponse.File();
-                svtTsdFile.setName(svcFileName + ".d.ts");
+                svtTsdFile.setName(svcFileName + '.d.ts');
                 svtTsdFile.setContent(TplEngine_1.TplEngine.render('svc_tsd', fileDescriptorModel));
                 codeGenResponse.addFile(svtTsdFile);
                 fileList.push(svcFileName);
@@ -56,23 +56,34 @@ Utility_1.Utility.withAllStdIn((inputBuff) => {
         }, []);
         const indexJsOutput = ProtoIndexFormatter_1.ProtoIndexFormatter.format(files, 'index_js');
         const indexJsFile = new plugin_pb_1.CodeGeneratorResponse.File();
-        indexJsFile.setName("index.js");
+        indexJsFile.setName('index.js');
         indexJsFile.setContent(indexJsOutput);
         codeGenResponse.addFile(indexJsFile);
         const indexTsOutput = ProtoIndexFormatter_1.ProtoIndexFormatter.format(files, 'index_tsd');
         const indexTsdFile = new plugin_pb_1.CodeGeneratorResponse.File();
-        indexTsdFile.setName("index.d.ts");
+        indexTsdFile.setName('index.d.ts');
         indexTsdFile.setContent(indexTsOutput);
         codeGenResponse.addFile(indexTsdFile);
         const interfaceTsOutput = ProtoIndexFormatter_1.ProtoIndexFormatter.format(interfaces, 'index_tsd');
         const interfaceTsdFile = new plugin_pb_1.CodeGeneratorResponse.File();
-        interfaceTsdFile.setName("interfaces.ts");
+        interfaceTsdFile.setName('interfaces.ts');
         interfaceTsdFile.setContent(interfaceTsOutput);
         codeGenResponse.addFile(interfaceTsdFile);
+        const noGrpcFiles = (filename) => !filename.includes('grpc');
+        const modelJsOutput = ProtoIndexFormatter_1.ProtoIndexFormatter.format(files.filter(noGrpcFiles), 'index_js');
+        const modelJsFile = new plugin_pb_1.CodeGeneratorResponse.File();
+        modelJsFile.setName('models.js');
+        modelJsFile.setContent(modelJsOutput);
+        codeGenResponse.addFile(modelJsFile);
+        const modelTsOutput = ProtoIndexFormatter_1.ProtoIndexFormatter.format(files.filter(noGrpcFiles), 'index_tsd');
+        const modelTsdFile = new plugin_pb_1.CodeGeneratorResponse.File();
+        modelTsdFile.setName('models.d.ts');
+        modelTsdFile.setContent(modelTsOutput);
+        codeGenResponse.addFile(modelTsdFile);
         process.stdout.write(new Buffer(codeGenResponse.serializeBinary()));
     }
     catch (err) {
-        console.error("protoc-gen-ts error: " + err.stack + "\n");
+        console.error('protoc-gen-ts error: ' + err.stack + '\n');
         process.exit(1);
     }
 });
